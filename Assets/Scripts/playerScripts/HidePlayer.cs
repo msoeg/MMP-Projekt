52fbe7e99edfe558fd338ca2052eab05630633b0
Melanie <135548773+EmlMeml@users.noreply.github.com>
2023-07-09 14:41:27 +0200

    Spieler verschwindet, jedoch egal wo

--- Assets/Scripts/playerScripts/HidePlayer.cs
+++ Assets/Scripts/playerScripts/HidePlayer.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class HidePlayer : MonoBehaviour
 {
-    private bool hidden;
+    public bool hidden;
+    public GameObject player;
+    SpriteRenderer playerRender;
     // Start is called before the first frame update
     void Start()
     {
         hidden = false;
+
+        if(player == null){
+            player = GameObject.Find("Player");
+            Debug.Log("new player Object: "+player.name);
+        } 
+        playerRender = player.GetComponent<SpriteRenderer>();
+        Debug.Log(playerRender.name);
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        Debug.Log("Before if-else");
         if(Input.GetKeyDown(KeyCode.E) && hidden == false){
             Debug.Log("Player is hidden!");
             hidden = true;
+            playerRender.enabled = false;
+            Debug.Log("Hidden: "+hidden);
         }else if(Input.GetKeyDown(KeyCode.E) && hidden == true){
             Debug.Log("Left Hidingspace!");
             hidden = false;
+            playerRender.enabled = true;
+            Debug.Log("Hidden: "+hidden);
         }
+        Debug.Log("After if-else");
 
     }
 }
