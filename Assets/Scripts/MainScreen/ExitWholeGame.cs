a35fd686c8df11e0f807ec2cd2d8c7a3bccaf433
simonroedig <simon.roedig@web.de>
2023-07-14 18:10:22 +0200

    Implement ExitWholeGame Button

---
+++ Assets/Scripts/MainScreen/ExitWholeGame.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ExitWholeGame : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
