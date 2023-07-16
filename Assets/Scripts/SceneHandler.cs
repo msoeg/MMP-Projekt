0712126f08fe9b0d825766e6e1dd8132f54fe400
Mario Soegtrop <m.soegtrop@campus.lmu.de>
2023-07-16 20:55:20 +0200

    Loads GameOver when EventCount >= 10

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -9,12 +9,15 @@ public class SceneHandler : MonoBehaviour
     private PlayerHealth _playerHealthObject;
     private Timer _timer;
     private string _remainingTime;
+    private EventCounter _eventCounter;
+    private int _eventCount;
     
     // Start is called before the first frame update
     void Start()
     {
         _playerHealthObject = GameObject.Find("Player").GetComponent<PlayerHealth>();
         _timer = GameObject.Find("GlobalTimeHandler").GetComponent<Timer>();
+        _eventCounter = GameObject.Find("EventCounter").GetComponent<EventCounter>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,12 @@ public class SceneHandler : MonoBehaviour
     {
         _playerHealth = _playerHealthObject.GetPlayerHealth();
         if (_playerHealth <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
+        _eventCount = _eventCounter.GetEventCount();
+        if (_eventCount >= 10)
         {
             SceneManager.LoadScene("GameOver");
         }
